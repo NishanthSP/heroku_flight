4f79ddc0de1f9f40eac434fb4addbe821328becf
NishanthSP <72077212+NishanthSP@users.noreply.github.com>
2021-01-07 23:52:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,37 +18,41 @@ def home():
 def blog():
 	return render_template('blog.html')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/code2'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-db = SQLAlchemy()
-db.init_app(app)
-
-
-class Contacts(db.Model):
-    '''
-    sno, name phone_num, msg, date, email
-    '''
-    sno = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-    phone_num = db.Column(db.String(12), nullable=False)
-    msg = db.Column(db.String(120), nullable=False)
-    date = db.Column(db.String(12), nullable=True)
-    email = db.Column(db.String(20), nullable=False)
-
-
-
-@app.route("/contact", methods = ['GET', 'POST'])
+@app.route('/contact)
 def contact():
-    if(request.method=='POST'):
-        '''Add entry to the database'''
-        name = request.form.get('name')
-        email = request.form.get('email')
-        phone = request.form.get('phone')
-        message = request.form.get('message')
-        entry = Contacts(name=name, phone_num = phone, msg = message, date= datetime.now(),email = email )
-        db.session.add(entry)
-        db.session.commit()
-    return render_template('contact.html')
+	return render_template('contact.html')
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/code2'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# db = SQLAlchemy()
+# db.init_app(app)
+
+
+# class Contacts(db.Model):
+#     '''
+#     sno, name phone_num, msg, date, email
+#     '''
+#     sno = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(80), nullable=False)
+#     phone_num = db.Column(db.String(12), nullable=False)
+#     msg = db.Column(db.String(120), nullable=False)
+#     date = db.Column(db.String(12), nullable=True)
+#     email = db.Column(db.String(20), nullable=False)
+
+
+
+# @app.route("/contact", methods = ['GET', 'POST'])
+# def contact():
+#     if(request.method=='POST'):
+#         '''Add entry to the database'''
+#         name = request.form.get('name')
+#         email = request.form.get('email')
+#         phone = request.form.get('phone')
+#         message = request.form.get('message')
+#         entry = Contacts(name=name, phone_num = phone, msg = message, date= datetime.now(),email = email )
+#         db.session.add(entry)
+#         db.session.commit()
+#     return render_template('contact.html')
 
 
 @app.route("/predict", methods = ["GET", "POST"])
